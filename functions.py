8f7f91c6e3cfbfca1e550d378413eb5251b09da4
Penaz91 <penaz@PenazMW.PenazNet>
2014-02-15 21:50:53 +0100

    Various Commits

--- functions.py
+++ functions.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+import os
 def Search(l,s):
-    return [x for x in l if s in x]
+    return [x for x in l if s.lower() in x.lower()]
+def ListDirs():
+    x=[os.path.join(dp,f) for dp, dn, fn in os.walk(os.path.expanduser("~/.password-store/")) for f in fn if ".gpg-id" not in f]
+    y=[os.path.relpath(d,".password-store") for d in x]
+    del x
+    return y
+def SetupEnv():
+    os.chdir(os.environ["HOME"])
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 import unittest
 import functions
+#The search must work correctly
 class SearchTest(unittest.TestCase):
     def test(self):
         x=["abc","abd","bdc","cdd"]
         y=functions.Search(x,"a")
         self.assertEqual(y,["abc","abd"])
+#I have a password store, so the list directory function must provide a non-empty list without gpg-id
+class ListTest(unittest.TestCase):
+    def testempty(self):
+        self.assertNotEqual(functions.ListDirs(),[])
+    def testid(self):
+        self.assertNotIn(".gpg-id",functions.ListDirs())
 if __name__=="__main__":
     unittest.main()
