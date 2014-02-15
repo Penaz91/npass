a6d9de278a4a91a3b2f8fd21434d307414435783
Penaz91 <penaz@PenazMW.PenazNet>
2014-02-15 22:59:37 +0100

    First Working Release

--- functions.py
+++ functions.py
@@ -6,5 +6,6 @@ def Search(l,s):
 def ListDirs():
     x=[os.path.join(dp,f) for dp, dn, fn in os.walk(os.path.expanduser("~/.password-store/")) for f in fn if ".gpg-id" not in f]
     y=[os.path.relpath(d,".password-store") for d in x]
-    del x
-    return y
+    x=[os.path.splitext(l)[0] for l in y]
+    del y
+    return x
