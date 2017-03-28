dc4694d83122b09198ffd73e41eac91c093ee751
Penaz91 <penazarea@altervista.org>
2017-03-28 12:42:08 +0200

    PEP-ification

--- functions.py
+++ functions.py
@@ -1,42 +1,49 @@
 #!/usr/bin/env python3
-#-----------------------------
+# -----------------------------
 # Functions.py
 # Part of the npass project
 # By Penaz
-#-----------------------------
+# -----------------------------
 
-#-----------------------------
+# -----------------------------
 # Imports
-#-----------------------------
+# -----------------------------
 from os.path import join, relpath, splitext, expanduser
 from os import walk
-#-----------------------------
+
+
+# -----------------------------
 # Filter the set "l"
-#-----------------------------
-def Search(l,s):
+# -----------------------------
+def Search(l, s):
     return {x for x in l if s.lower() in x.lower()}
-#-----------------------------
+
+
+# -----------------------------
 # List all passwords
-#-----------------------------
+# -----------------------------
 def ListDirs():
     exclude = set([".gpg-id", ".git"])
-    x={join(dp,f) for dp, dn, fn in walk(expanduser("~/.password-store/")) for f in fn}
-    toexclude=set([])
+    x = {join(dp, f) for dp, dn, fn in walk(expanduser("~/.password-store/"))
+         for f in fn}
+    toexclude = set([])
     for exclusion in exclude:
         toexclude = set([item for item in x if exclusion in item])
-        x=x.difference(toexclude)
-    y={relpath(d,".password-store") for d in x}
-    x={splitext(l)[0] for l in y}
+        x = x.difference(toexclude)
+    y = {relpath(d, ".password-store") for d in x}
+    x = {splitext(l)[0] for l in y}
     del y
     return x
-#-----------------------------
+
+
+# -----------------------------
 # Display condensed paths if necessary
-#-----------------------------
-def ShortenPath(string,rowl):
-    if len(string)>rowl-5:
-        #Shorten the string
-        l=string.split("/")
-        return join(join(l[0],*[".." for i in range(len(l)-2)]),l[len(l)-1])
+# -----------------------------
+def ShortenPath(string, rowl):
+    if len(string) > rowl-5:
+        # Shorten the string
+        l = string.split("/")
+        return join(join(l[0], *[".." for i in range(len(l)-2)]), l[len(l)-1])
     else:
-        #Return the same string
+        # Return the same string
         return string
--- main.py
+++ main.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python3
-#--------------------------------------------------
+# --------------------------------------------------
 # nPass
 # A simple NCurses frontend for Pass
 # By Penaz
-#--------------------------------------------------
-#--------------------
+# --------------------------------------------------
+# --------------------
 # Imports
-#--------------------
+# --------------------
 import curses
 import functions
 from os import chdir, environ
 from sys import argv
 from subprocess import call, Popen
-#--------------------
+# --------------------
 # Environment Setup
-#--------------------
-chdir(environ["HOME"]) 
-#--------------------
+# --------------------
+chdir(environ["HOME"])
+# --------------------
 # Vars
-#--------------------
-s=""
-pos=0
-l=functions.ListDirs()
-ignore_keys=set.union({f for f in range(265,328)},{262,260,261,ord("\t"),curses.KEY_IC})
-stack=[]
-mode=0      #0=Open - 1=Copy to clipboard - 2=Edit - 3=Delete
-#--------------------
+# --------------------
+running = True
+s = ""
+pos = 0
+l = functions.ListDirs()
+ignore_keys = set.union({f for f in range(265, 328)}, {262, 260,
+                        261, ord("\t"), curses.KEY_IC})
+stack = []
+mode = 0      # 0=Open - 1=Copy to clipboard - 2=Edit - 3=Delete
+# --------------------
 # Argument detection
-#--------------------
-if len(argv)>1:
-    if argv[1] in {"-c","--clip","--copy"}:
-        mode=1
-    if argv[1] in {"-e","--edit"}:
-        mode=2
-    if argv[1] in {"-d","--delete"}:
-        mode=3
-    if argv[1] in {"-h","--help"}:
+# --------------------
+if len(argv) > 1:
+    if argv[1] in {"-c", "--clip", "--copy"}:
+        mode = 1
+    if argv[1] in {"-e", "--edit"}:
+        mode = 2
+    if argv[1] in {"-d", "--delete"}:
+        mode = 3
+    if argv[1] in {"-h", "--help"}:
         print("Npass - A simple ncurses frontend for Pass")
         print("Usage:")
         print("npass  -  Show password list")
@@ -44,156 +46,162 @@ if len(argv)>1:
         print("npass [-d,--delete]  -  Open npass in delete mode")
         print("npass [-h,--help]  -  Show this help page")
         quit()
-if len(argv)>2:
+if len(argv) > 2:
     quit("Too many arguments\nTry with fewer arguments.")
-#--------------------
+# --------------------
 # Curses Init
-#--------------------
-screen=curses.initscr()
+# --------------------
+screen = curses.initscr()
 screen.border()
 curses.noecho()
 curses.cbreak()
 screen.keypad(1)
-#--------------------
+
+
+# --------------------
 # Termination handler
-#--------------------
+# --------------------
 def term():
     curses.nocbreak()
-    screen.keypad(False)
+    screen.keypad(0)
     curses.echo()
     curses.endwin()
-#--------------------
+
+
+# --------------------
 # Curses Windows
-#--------------------
-dim=screen.getmaxyx()
-txtwin=curses.newwin(3,dim[1]-3,dim[0]-3,1)
+# --------------------
+dim = screen.getmaxyx()
+txtwin = curses.newwin(3, dim[1]-3, dim[0]-3, 1)
 txtwin.border()
-passwin=curses.newpad(len(l)+2,dim[1]-3)
-scroll=dim[0]-10<len(l)+2
+passwin = curses.newpad(len(l)+2, dim[1]-3)
+scroll = dim[0]-10 < len(l)+2
 curses.start_color()
 curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-#--------------------
+# --------------------
 # Curses Loop
-#--------------------
-while True:
-    screen.addstr(2,int((dim[1]-5)/2),"nPass",curses.A_BOLD)
-    if mode==0:
-        screen.addstr(3,int((dim[1]-13)/2),"<- Display ->",curses.A_BOLD)
-    elif mode==1:
-        screen.addstr(3,int((dim[1]-10)/2),"<- Copy ->",curses.A_BOLD)
-    elif mode==2:
-        screen.addstr(3,int((dim[1]-10)/2),"<- Edit ->",curses.A_BOLD)
-    elif mode==3:
-        screen.addstr(3,int((dim[1]-18)/2),"<- !!! Delete !!! ->",curses.color_pair(1))
-    screen.hline(4,1,curses.ACS_HLINE,dim[1]-2)
-    k=sorted(list(l))
+# --------------------
+while running:
+    screen.addstr(2, int((dim[1]-5)/2), "nPass", curses.A_BOLD)
+    if mode == 0:
+        screen.addstr(3, int((dim[1]-13)/2), "<- Display ->", curses.A_BOLD)
+    elif mode == 1:
+        screen.addstr(3, int((dim[1]-10)/2), "<- Copy ->", curses.A_BOLD)
+    elif mode == 2:
+        screen.addstr(3, int((dim[1]-10)/2), "<- Edit ->", curses.A_BOLD)
+    elif mode == 3:
+        screen.addstr(3, int((dim[1]-18)/2), "<- !!! Delete !!! ->",
+                      curses.color_pair(1))
+    screen.hline(4, 1, curses.ACS_HLINE, dim[1]-2)
+    k = sorted(list(l))
     for n in range(len(l)):
-        if n==pos:
-            passwin.addstr(n,3,functions.ShortenPath(k[n],dim[1]),curses.A_REVERSE)
+        if n == pos:
+            passwin.addstr(n, 3, functions.ShortenPath(k[n], dim[1]),
+                           curses.A_REVERSE)
         else:
-            passwin.addstr(n,3,functions.ShortenPath(k[n],dim[1]))
-    #--------------------
+            passwin.addstr(n, 3, functions.ShortenPath(k[n], dim[1]))
+    # --------------------
     # Screen Refresh
-    #--------------------
+    # --------------------
     txtwin.border()
-    txtwin.addstr(1,3,"Search >>> ")
+    txtwin.addstr(1, 3, "Search >>> ")
     screen.border()
     screen.refresh()
     txtwin.refresh()
-    if pos>=(dim[0]-5)/4 and scroll:
-        passwin.refresh(int(pos-(dim[0]-5)/4),1,5,1,dim[0]-5,dim[1]-3)
+    if pos >= (dim[0]-5)/4 and scroll:
+        passwin.refresh(int(pos-(dim[0]-5)/4), 1, 5, 1, dim[0]-5, dim[1]-3)
     else:
-        passwin.refresh(0,1,5,1,dim[0]-5,dim[1]-3)
-    #--------------------
+        passwin.refresh(0, 1, 5, 1, dim[0]-5, dim[1]-3)
+    # --------------------
     # Capture KeyPresses
-    #--------------------
-    c=screen.getch()
+    # --------------------
+    c = screen.getch()
     screen.clear()
     txtwin.clear()
     passwin.clear()
-    if c==27:
-        #----------------------------------------
+    if c == 27:
+        # ----------------------------------------
         # Escape: Terminate
-        #----------------------------------------
-        break
-    elif c==127 or c==curses.KEY_DC:
-        #----------------------------------------
+        # ----------------------------------------
+        running = False
+    elif c == 127 or c == curses.KEY_DC:
+        # ----------------------------------------
         # Backspace/Delete Char: pop old content from stack
-        #----------------------------------------
-        pos=0
-        s=s[:-1]
-        if len(stack)>0:
-           l=set.union(l,stack.pop())
-        txtwin.addstr(1,3,"Search >>>  "+s)
-    elif c==259 or c==curses.KEY_PPAGE:
-        #----------------------------------------
+        # ----------------------------------------
+        pos = 0
+        s = s[:-1]
+        if len(stack) > 0:
+            l = set.union(l, stack.pop())
+        txtwin.addstr(1, 3, "Search >>>  "+s)
+    elif c == 259 or c == curses.KEY_PPAGE:
+        # ----------------------------------------
         # Up Arrow/PGUP: Go up in the menu
-        #----------------------------------------
-        if pos==0:
-            pos=len(l)-1
+        # ----------------------------------------
+        if pos == 0:
+            pos = len(l)-1
         else:
-            pos-=1
-    elif c==258 or c==curses.KEY_NPAGE:
-        #----------------------------------------
+            pos -= 1
+    elif c == 258 or c == curses.KEY_NPAGE:
+        # ----------------------------------------
         # Down Arrow: Go Down in the menu
-        #----------------------------------------
-        if pos==len(l)-1:
-            pos=0
+        # ----------------------------------------
+        if pos == len(l)-1:
+            pos = 0
         else:
-            pos+=1
-    elif c==10:
-        #----------------------------------------
+            pos += 1
+    elif c == 10:
+        # ----------------------------------------
         # Enter/Return: <action> password
-        #----------------------------------------
-        if len(l)==0:
+        # ----------------------------------------
+        if len(l) == 0:
             pass
         else:
-            k=sorted(list(l))
-            if mode==0:
+            k = sorted(list(l))
+            if mode == 0:
                 term()
-                Popen(["pass",k[pos]])
-            if mode==1:
+                Popen(["pass", k[pos]])
+            if mode == 1:
                 term()
-                call(["pass","-c",k[pos]])
-            elif mode==2:
+                call(["pass", "-c", k[pos]])
+            elif mode == 2:
                 term()
-                call(["pass","edit",k[pos]])
-            elif mode==3:
+                call(["pass", "edit", k[pos]])
+            elif mode == 3:
                 term()
-                call(["pass","rm",k[pos]])
-            break
-    elif c==260:
-        #----------------------------------------
+                call(["pass", "rm", k[pos]])
+            running = False
+    elif c == 260:
+        # ----------------------------------------
         # Left arrow: Change mode left
-        #----------------------------------------
-        mode-=1
-        if mode<0:
-            mode=3
-    elif c==261:
-        #----------------------------------------
+        # ----------------------------------------
+        mode -= 1
+        if mode < 0:
+            mode = 3
+    elif c == 261:
+        # ----------------------------------------
         # Right arrow: Change mode Right
-        #----------------------------------------
-        mode+=1
-        if mode>3:
-            mode=0
-        #----------------------------------------
+        # ----------------------------------------
+        mode += 1
+        if mode > 3:
+            mode = 0
+        # ----------------------------------------
         # Ignore some Keys
-        #----------------------------------------
+        # ----------------------------------------
     elif c in ignore_keys:
-        txtwin.addstr(1,3,"Search >>>  "+s)
+        txtwin.addstr(1, 3, "Search >>>  "+s)
     else:
-        #----------------------------------------
+        # ----------------------------------------
         # Letters/Numbers: perform search
-        #----------------------------------------
-        pos=0
-        s+=chr(c)
-        txtwin.addstr(1,3,"Search >>>  "+s)
-        oldl=l
-        l=functions.Search(l,s)
+        # ----------------------------------------
+        pos = 0
+        s += chr(c)
+        txtwin.addstr(1, 3, "Search >>>  "+s)
+        oldl = l
+        l = functions.Search(l, s)
         stack.append(oldl-l)
         del oldl
-#--------------------------------------------------
-#Program Termination & Cleanup
-#--------------------------------------------------
+# --------------------------------------------------
+# Program Termination & Cleanup
+# --------------------------------------------------
 term()
 quit()
